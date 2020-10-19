5da6c0475083e62b34ba116e92f0b110a08ba139
hoomanist <hoomanist86@protonmail.com>
2020-10-19 20:16:11 +0330

    return to each function database stablizer

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/hoomanist/foodly
 go 1.15
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/mux v1.8.0
 	go.mongodb.org/mongo-driver v1.4.2
 	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897
 )
--- main.go
+++ main.go
@@ -14,8 +14,6 @@ import (
   "go.mongodb.org/mongo-driver/bson"
 )
 
-var client *mongo.Client
-
 // check it is okay
 func ok(w http.ResponseWriter, req *http.Request){
   w.Header().Add("content-type", "application/json")
@@ -27,12 +25,18 @@ func ok(w http.ResponseWriter, req *http.Request){
 
 // register
 func register(w http.ResponseWriter, req *http.Request) {
-  ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-  defer cancel()
   log.Println("contacts /register")
   w.Header().Add("content-type", "application/json")
   var t RegisterRequest
-  err := json.NewDecoder(req.Body).Decode(&t)
+  ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+  defer cancel()
+  client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+  defer client.Disconnect(ctx)
+  err = client.Ping(ctx, readpref.Primary())
+  if err != nil {
+    http.Error(w, "Unable to connect to db. Please try again later!", http.StatusInternalServerError)
+  }
+  err = json.NewDecoder(req.Body).Decode(&t)
   if err != nil {
     http.Error(w, "unable to parse json", http.StatusBadRequest)
   }
@@ -52,7 +56,13 @@ func login(w http.ResponseWriter, req *http.Request){
   w.Header().Add("content-type", "application/json")
   ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
   defer cancel()
-  err := json.NewDecoder(req.Body).Decode(&t)
+  client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+  defer client.Disconnect(ctx)
+  err = client.Ping(ctx, readpref.Primary())
+  if err != nil {
+    http.Error(w, "Unable to connect to db. Please try again later!", http.StatusInternalServerError)
+  }
+  err = json.NewDecoder(req.Body).Decode(&t)
   if err != nil {
     http.Error(w, "Wrong Request", http.StatusBadRequest)
   }
@@ -65,14 +75,6 @@ func login(w http.ResponseWriter, req *http.Request){
 }
 
 func main() {
-  ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-  defer cancel()
-  client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
-  defer client.Disconnect(ctx)
-  err = client.Ping(ctx, readpref.Primary())
-  if err != nil {
-    panic("database error")
-  }
   r := mux.NewRouter()
   r.HandleFunc("/login", login).Methods("POST")
   r.HandleFunc("/register", register).Methods("POST")
