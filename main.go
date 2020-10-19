3f101137a8ef28f7efb1d38263dc9397e70eeefe
hoomanist <hoomanist86@protonmail.com>
2020-10-19 12:18:53 +0330

    ok request

--- main.go
+++ main.go
@@ -2,8 +2,19 @@ package main
 
 import (
   "fmt"
+  "net/http"
+  "log"
+  "encoding/json"
 )
 
+func ok(w http.ResponseWriter, req *http.Request){
+  log.Printf("%s contacts /ok", req.RemoteAddr)
+  data := map[string]string{"status": "ok"}
+  res, _ := json.Marshal(data)
+  fmt.Fprintf(w, string(res))
+}
+
 func main() {
-  fmt.Println("Hello, World !")
+  http.HandleFunc("/ok", ok)
+  http.ListenAndServe(":5000", nil)
 }
